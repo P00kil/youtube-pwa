fbce5c868c0d079fc0c9fb26e727c053a7f29744
P00kil <166103221+P00kil@users.noreply.github.com>
2025-02-17 21:04:22 +0100

    Delete service-worker.js directory

--- service-worker.js/service-worker.js
+++
@@ -1,8 +0,0 @@
-// service-worker.js
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installiert.");
-});
-
-self.addEventListener("fetch", (event) => {
-    console.log("Anfrage:", event.request.url);
-});
